fe23388b25a6d207e67e89a9186c857ee03b7b8d
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-10-29 20:47:15 +0300

    Add project files.

---
+++ Tyuiu.BorisovaDS.Sprint3.Task0.V7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BorisovaDS.Sprint3.Task0.V7.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint3.Task0.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Борисова Д.С. | ИСТН-б-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнила: Борисова Диана Сергеевна | ИСТН-б-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда по  *");
+            Console.WriteLine("* формуле, при a = 0,75                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double a = 0.75;
+            int startValue = 1, stopValue = 20;
+            Console.WriteLine("Переменная A = " + a);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма ряда " + ds.GetSumSeries(a, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
