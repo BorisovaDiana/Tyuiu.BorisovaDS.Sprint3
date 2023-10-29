305ffd52687abb56cfdf647f0636e660278a8f6d
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-10-29 23:30:44 +0300

    Task7

---
+++ Tyuiu.BorisovaDS.Sprint3.Task7.V16.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BorisovaDS.Sprint3.Task7.V16.Lib
+{
+    public class DataService : ISprint3Task7V16
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray; // объявляем пустой массив
+            int len = (stopValue - startValue) + 1; // вычисляем длину массива
+            valueArray = new double[len]; // создаем массив с указанной длиной
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(Math.Sin(x), 2); // округляем значение до 2 знаков после запятой 
+                valueArray[count] = y; // присваеваем значение в массив с индексом count
+                count++;
+
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.BorisovaDS.Sprint3.Task7.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BorisovaDS.Sprint3.Task7.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BorisovaDS.Sprint3.Task7.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("001d7a85-be30-429d-ace9-465222989818")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
