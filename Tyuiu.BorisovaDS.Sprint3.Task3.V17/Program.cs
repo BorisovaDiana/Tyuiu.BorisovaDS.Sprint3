c7c70a65d7cfc75926e01000d8e057ad83945357
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-10-29 21:52:42 +0300

    Task3

---
+++ Tyuiu.BorisovaDS.Sprint3.Task3.V17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BorisovaDS.Sprint3.Task3.V17.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint3.Task3.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнила: Борисова Д.С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнила: Борисова Диана Сергеевна | ИСТНб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach удалить все буквы и знаки препинания,            *");
+            Console.WriteLine("* оставить цифру затем преобразовать в число в строке: *vn98n! b,         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            string value = "*vn98n! b,";
+            int res = ds.ConvertStringToInt(value);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("После удаления всех букв и знаков препинания, осталось только число: " + res);
+            Console.ReadKey();
+        }
+    }
+}
